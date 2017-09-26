5443ff365c59b46905e7a1d5d711a7f788e545fc
Tomohisa Uchida <uddystyle@gmail.com>
2017-09-26 20:43:27 +0900

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -12,7 +12,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # 新しいセッションのフォームが正しく表示されたことを確認する
     assert_template 'sessions/new'
     # わざと無効なparamsハッシュを使ってセッション用パスにPOSTする
-    post login_path, params: { session: { email: "", password: ""} }
+    post login_path, params: { session: { email: "", password: "" } }
     # 新しいセッションのフォームが再度表示され、フラッシュメッセージが追加されることを確認する
     assert_template 'sessions/new'
     assert_not flash.empty?
@@ -25,7 +25,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email: @user.email,
-    password: 'password'} }
+                                          password: 'password' } }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -35,9 +35,26 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_not is_logged_in?
+    assert_redirected_to root_url
+    # ２番めのウインドウでログアウトをクリックするユーザーをシュミレートする
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
